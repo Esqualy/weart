ea88ed3532d3e1380b724d69fbb99eb7001b64ed
ThsArtiste <71966984+ThsArtiste@users.noreply.github.com>
2025-03-13 12:13:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,9 @@ import numpy as np
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-L/14", device=device)
 #ViT-B/16
-image_paths = [
-    "oeuvre1.jpg",  # Chaise 1
-    "oeuvre2.jpg",  # Chaise 2
-    "oeuvre3.jpg",  # Table 1
-    "oeuvre4.jpg",  # Table 2
-    "oeuvre5.jpg",  # Avion 1
-    "oeuvre6.jpg"   # Avion 2
-]
+
+image_paths = [oeuvre["path"] for oeuvre in oeuvres]
+image_labels = [oeuvre["titre"] for oeuvre in oeuvres]
 
 # Charger et prétraiter les images
 images = [preprocess(Image.open(img)).unsqueeze(0).to(device) for img in image_paths]
@@ -31,15 +26,6 @@ similarity_matrix = image_features @ image_features.T  # Produit scalaire des em
 print("Matrice des similarités entre images :")
 similarity_matrix_np = similarity_matrix.cpu().numpy()
 print(similarity_matrix_np)
-
-images_labels = [
-    "Chaise 1",
-    "Chaise 2",
-    "Table 1",
-    "Table 2",
-    "Avion 1",
-    "Avion 2"
-]
 
 similar_images = {}
 
