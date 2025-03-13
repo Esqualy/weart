51f5a145265757078d8c8c6afa75217032b27428
Thibault - Esqualy <74820413+Esqualy@users.noreply.github.com>
2025-03-13 10:29:03 +0100

    Algo IA - Modification
    Ajout d'une boucle détectant à partir d'un certain seuil, les oeuvres qui se ressemble et le stock dans une variable.

--- app.py
+++ app.py
@@ -5,48 +5,48 @@ import numpy as np
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-L/14", device=device)
-
+#ViT-B/16
 image_paths = [
-    "oeuvre1.jpg",
-    "oeuvre2.jpg",
-    "oeuvre3.jpg",
-    "oeuvre4.jpg",
-    "oeuvre5.jpg",
-    "oeuvre6.jpg"
+    "oeuvre1.jpg",  # Chaise 1
+    "oeuvre2.jpg",  # Chaise 2
+    "oeuvre3.jpg",  # Table 1
+    "oeuvre4.jpg",  # Table 2
+    "oeuvre5.jpg",  # Avion 1
+    "oeuvre6.jpg"   # Avion 2
 ]
 
+# Charger et prétraiter les images
 images = [preprocess(Image.open(img)).unsqueeze(0).to(device) for img in image_paths]
 
+# Extraire les embeddings - representation vectorielle 
 with torch.no_grad():
     image_features = torch.cat([model.encode_image(img) for img in images])
 
+# Normaliser les embeddings
 image_features /= image_features.norm(dim=-1, keepdim=True)
 
-similarity_matrix = image_features @ image_features.T
+# Calculer la similarité cosinus entre les images
+similarity_matrix = image_features @ image_features.T  # Produit scalaire des embeddings
 
 print("Matrice des similarités entre images :")
 similarity_matrix_np = similarity_matrix.cpu().numpy()
 print(similarity_matrix_np)
 
 images_labels = [
-    "Les Tournesols - Van Gogh",
-    "La Nuit étoilée - Van Gogh",
-    "Impression, soleil levant - Monet",
-    "Les Demoiselles d’Avignon - Picasso",
-    "La Persistance de la mémoire - Dalí",
-    "Composition VIII - Kandinsky"
+    "Chaise 1",
+    "Chaise 2",
+    "Table 1",
+    "Table 2",
+    "Avion 1",
+    "Avion 2"
 ]
 
+similar_images = {}
+
 for i in range(len(image_paths)):
-    for j in range(i+1, len(image_paths)):
+    res = []
+    for j in range(i + 1, len(image_paths)):
         similarity = similarity_matrix_np[i][j]
-        print(f"\nSimilarité entre {images_labels[i]} et {images_labels[j]} : {similarity:.4f}")
-        
-        if similarity > 0.75:
-            print("Ces œuvres sont très similaires !")
-        elif similarity > 0.65:
-            print("Ces œuvres sont assez similaires.")
-        elif similarity > 0.55:
-            print("Il y a une certaine ressemblance, mais elles sont différentes.")
-        else:
-            print("Ces œuvres sont peu similaires.")
+        if similarity > 0.65: 
+            res.append((images_labels[j], similarity))  
+    similar_images[images_labels[i]] = res 
