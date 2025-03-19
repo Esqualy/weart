8a1901afe793eb9090da361082739d8de8569276
Thibault - Esqualy <74820413+Esqualy@users.noreply.github.com>
2025-03-19 20:32:13 +0100

    Update server.py

--- website/server.py
+++ website/server.py
@@ -4,7 +4,7 @@ import json
 import os
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  # Nécessaire pour utiliser les sessions
+app.secret_key = 'esqualycookie'  
 
 # Vérification du mot de passe avec bcrypt
 def verify_password(input_password, stored_hash):
