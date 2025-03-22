be0f5e940130e88efb6e1a13b7d71a9b29bdf40b
Thibault - Esqualy <74820413+Esqualy@users.noreply.github.com>
2025-03-22 13:45:44 +0100

    Update app.py

--- cdn/app.py
+++ cdn/app.py
@@ -59,14 +59,54 @@ def serve_files(filename):
         except FileNotFoundError:
             sftp.close()
             transport.close()
-            return render_template('404.html'), 404  
+            return render_template('not_found.html'), 404  
 
     except Exception as e:
-        return f"Erreur d'accès au serveur SFTP : {e}", 500
+        return render_template('internal_server_error.html', error=str(e)), 500  
+
+@app.errorhandler(400)
+def bad_request(error):
+    return render_template('bad_request.html'), 400  
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return render_template('unauthorized.html'), 401 
+
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template('forbidden.html'), 403  
 
 @app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404  
+def not_found(error):
+    return render_template('not_found.html'), 404  
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return render_template('method_not_allowed.html'), 405  
+
+@app.errorhandler(406)
+def not_acceptable(error):
+    return render_template('not_acceptable.html'), 406  
+
+@app.errorhandler(412)
+def precondition_failed(error):
+    return render_template('precondition_failed.html'), 412  
+
+@app.errorhandler(415)
+def unsupported_media_type(error):
+    return render_template('unsupported_media_type.html'), 415  
+
+@app.errorhandler(501)
+def not_implemented(error):
+    return render_template('not_impremented.html'), 501  
+
+@app.errorhandler(502)
+def bad_gateway(error):
+    return render_template('bad_gateway.html'), 502  
+
+@app.errorhandler(503)
+def maintenance(error):
+    return render_template('maintenance.html'), 503 
 
 if __name__ == '__main__':
     app.run(debug=True)
