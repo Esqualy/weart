ce5a1b61f4cc1b48e50f80a75493d92e65191235
Stef Meden <58486072+smeden-lod@users.noreply.github.com>
2025-03-23 18:32:43 +0100

    Update requetes.py
    un bug à résoudre dans la dernière def

--- requetes.py
+++ requetes.py
@@ -1,6 +1,5 @@
 import json
 
-
 def like(IdAm, IdOeu):
     """
     Système de like qui prends en entrée l'amateur et l'oeuvre qu'il a liké, pour créer une nouvelle ligne dans "amateur_oeuvre.json"
@@ -15,17 +14,14 @@ def like(IdAm, IdOeu):
         json.dump(data,f)
     print("données update")
 
-
 def like_amateur (IdAm):
     """Prends en entrée l'id de l'amateur et renvoie les id des oeuvres qu'il a liké."""
-
     res = []
     with open("amateur_oeuvre.json","r") as f:
         data = json.load(f)
         for d in data:
             if d["IdAm"] == IdAm:
                 res.append(d["IdOeu"])
-
 
 def auteur(IdOeu):
     """Prends en entrée l'id d'une oeuvre et renvoie l'id de son artiste."""
@@ -36,14 +32,12 @@ def auteur(IdOeu):
                 return d["IdAr"]
     return None
 
-
 def oeuvres_auteur(IdAr):
     """Prends en entrée l'id  d'un artiste, et renvoie les ids des oeuvres qu'il a créées."""
     with open("oeuvres.json", "r") as f:
         data = json.load(f)
         IdAr = str(IdAr)
     return [d["IdOeu"] for d in data if d["IdAr"] == IdAr]
-
 
 def oeuvres_auteurs(artists):
     """Prend en entrée une liste d'id d'artiste, renvoie la liste des id des oeuvres de ces artistes
@@ -52,7 +46,6 @@ def oeuvres_auteurs(artists):
     for x in artists:
         res.extend(oeuvres_auteur(x))
     return res
-
 
 def like_oeuvre(IdOeu):
     with open("amateur_oeuvre.json", "r") as f:
@@ -66,10 +59,6 @@ def like_oeuvre(IdOeu):
         else:
             return "Aucun amateur n'a aimé cette œuvre"
       
-        
-
-
-
 def oeuvres_likées_amateur(IdAm):
     """Prends en entrée l'id d'un amateur, regarde les amateurs ayant liké les mêmes œuvres que lui, et renvoie leurs œuvres likées."""
     oeuvres_likées = like_amateur(IdAm)
@@ -79,9 +68,6 @@ def oeuvres_likées_amateur(IdAm):
         for d in data:
             if d["IdOeu"] in oeuvres_likées and d["IdAm"] != IdAm:
                 amateurs_ayant_les_mêmes_oeuvres.append(d["IdAm"])
-
     # Récupérer toutes les œuvres likées par ces autres amateurs
     oeuvres_likées_par_autres_amateurs = oeuvres_auteurs(list(amateurs_ayant_les_mêmes_oeuvres))
-    return ("IdOeu")
-
-print(oeuvres_likées_amateur(1))
+    return ("IdOeu")  # sans doute pas ?
